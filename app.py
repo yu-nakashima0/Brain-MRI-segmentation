9cf4b0d563d2e4d4f920aaaa9c4029803f93d9ea
yu-nakashima0 <yu.nakashima@stud.uni-hannover.de>
2025-09-19 16:20:55 +0200

    DataFrame corrected

--- app.py
+++ app.py
@@ -14,24 +14,45 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 #dataset_ref = 'b33mvssrimanth/kaggle-3m'
 #api.dataset_download_files(dataset_ref, path='datasets/kaggle-3m', unzip=True)
 
-BASE_LEN = 89
-END_IMG_LEN = 4 
-END_MASK_LEN = 9 
-IMG_SIZE = 512
-
 DATA_PATH = "./datasets/kaggle-3m/kaggle_3m"
 
 filenames = []
 fotos = []
+imgs = [] 
+masks = []
+patients = []
 for root, dirs, files in os.walk(DATA_PATH):
     for filename in files:
         full_path = os.path.join(root, filename)
         #print(full_path)
         filenames.append(root)
         fotos.append(full_path)
+        name_of_patient = root[31:43]
+        patients.append(name_of_patient)
+        if "mask" in full_path:
+            masks.append(full_path)
+            imgs.append(0)
+        else:
+            imgs.append(full_path)
+            masks.append(0)
 
 df = pd.DataFrame({
-    "filename": filenames, 
-    "foto": fotos
+    "Patient": patients, 
+    "Filename": filenames, 
+    "Photo": fotos,
+    "Image": imgs,
+    "Mask": masks,
 })
 print(df.head())
+print(df.info())
+print(df.describe())
+print(df.columns)
+
+data = pd.read_csv("./data.csv")
+
+df = pd.merge(df, data, on=["Patient"], how="outer")
+print(df.info())
+print(df.describe())
+print(df.head())
+print(df.tail())
+print(df.columns)
