8238c511329a1958df4706bfebe2ee4c41c1ddfa
yu-nakashima0 <yu.nakashima@stud.uni-hannover.de>
2025-09-19 17:26:05 +0200

    DataFrame corrected and add new feature Diagnosis

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
+import cv2
 import glob
+import seaborn as sns
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,6 +16,7 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 #dataset_ref = 'b33mvssrimanth/kaggle-3m'
 #api.dataset_download_files(dataset_ref, path='datasets/kaggle-3m', unzip=True)
 
+#read dataset
 DATA_PATH = "./datasets/kaggle-3m/kaggle_3m"
 
 filenames = []
@@ -27,32 +30,56 @@ for root, dirs, files in os.walk(DATA_PATH):
         #print(full_path)
         filenames.append(root)
         fotos.append(full_path)
-        name_of_patient = root[31:43]
-        patients.append(name_of_patient)
         if "mask" in full_path:
+            name_of_patient = root[31:43]
+            patients.append(name_of_patient)
             masks.append(full_path)
-            imgs.append(0)
         else:
             imgs.append(full_path)
-            masks.append(0)
 
-df = pd.DataFrame({
-    "Patient": patients, 
-    "Filename": filenames, 
-    "Photo": fotos,
+
+#make dataframe
+df_photo = pd.DataFrame({
+    "Patient": patients,
     "Image": imgs,
-    "Mask": masks,
+    "Mask" : masks
 })
-print(df.head())
-print(df.info())
-print(df.describe())
-print(df.columns)
+print(df_photo.isna().sum())
 
+#read csv data 
 data = pd.read_csv("./data.csv")
+print(data.isna().sum())
+print(data.duplicated().sum())
 
-df = pd.merge(df, data, on=["Patient"], how="outer")
+
+#merge df_photo and data
+df = pd.merge(df_photo, data, on=["Patient"], how="outer")
 print(df.info())
 print(df.describe())
 print(df.head())
 print(df.tail())
 print(df.columns)
+
+#check duplicate and missing value
+print(df.isna().sum())
+print(df.duplicated().sum())
+
+# Adding A/B column for diagnosis
+def positiv_negativ_diagnosis(masks):
+    value = np.max(cv2.imread(masks))
+    if value > 0 : 
+        return 1
+    else: 
+        return 0
+
+#make new feature "Diagnosis"
+df["Diagnosis"] = df["Mask"].apply(lambda m: positiv_negativ_diagnosis(m))
+print(df.head())
+
+
+sns.countplot(data=df, x = "Diagnosis", palette=["paleturquoise", "salmon"])
+plt.xticks(ticks=[0,1], labels=["Negativ", "Positiv"])
+plt.title("Tumor Detected / Not Detected")
+plt.show()
+
+
